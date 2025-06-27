6a6df9955b4dbc96ebff777cb82c0d13ae3e639e
Twan Braas <twan.braas@mediamonks.com>
2025-06-27 16:09:45 +0200

    Update footer styling

--- source/frontend/src/components/cms-components/footer/Footer.tsx
+++ source/frontend/src/components/cms-components/footer/Footer.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { ensuredForwardRef, type MutableRefs, useRefs } from '@mediamonks/react-kit';
+import classNames from 'clsx';
 import Link from 'next/link';
-import type { ReactElement } from 'react';
+import { usePathname } from 'next/navigation';
+import { type ReactElement, useMemo } from 'react';
 import FooterLogo from '../../icons/footer-logo.svg';
 import styles from './Footer.module.scss';
 import type { FooterProps } from './Footer.types';
@@ -12,25 +14,52 @@ export type FooterRefs = MutableRefs<{
 }>;
 
 export const Footer = ensuredForwardRef<HTMLElement, FooterProps>(
-  ({ links = [], copyrightLeft, copyrightRight }, ref): ReactElement => {
+  (
+    { links = [], copyrightLeft, copyrightRight, footerVariant = 'darkBackground' },
+    ref,
+  ): ReactElement => {
     const refs = useRefs<FooterRefs>({
       self: ref,
     });
+    const pathname = usePathname();
+
+    const linksWithActive = useMemo(
+      () =>
+        links.map((item) => ({
+          ...item,
+          isActive: pathname.includes(item.href),
+        })),
+      [links, pathname],
+    );
 
     return (
-      <footer ref={refs.self} className={styles.footer}>
+      <footer ref={refs.self} className={classNames(styles.footer, styles[footerVariant])}>
         <div className={styles.footerContent}>
           <div className={styles.linksWrapper}>
-            {links.length > 0 &&
-              links.map((link, index) => (
+            <div className={styles.leftLinks}>
+              {linksWithActive.slice(0, Math.ceil(links.length / 2)).map((link, index) => (
                 <Link
                   // eslint-disable-next-line react/no-array-index-key
-                  key={`footer-link-${index}`}
+                  key={`left-link-${index}`}
                   href={link.href}
+                  className={classNames(link.isActive && styles.active, styles.link)}
                 >
                   {link.children}
                 </Link>
               ))}
+            </div>
+            <div className={styles.rightLinks}>
+              {linksWithActive.slice(Math.ceil(links.length / 2)).map((link, index) => (
+                <Link
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={`right-link-${index}`}
+                  href={link.href}
+                  className={classNames(link.isActive && styles.active, styles.link)}
+                >
+                  {link.children}
+                </Link>
+              ))}
+            </div>
           </div>
 
           <div className={styles.logoSection}>
